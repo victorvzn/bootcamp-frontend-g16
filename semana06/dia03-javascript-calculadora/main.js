91fb821def5c72301836ee634e0e5859297aeec1
Victor Villazón <victorzn23@gmail.com>
2023-11-04 21:37:22 -0500

    semana06, dia03, javasript-calculadora

--- semana06/dia03-javascript-calculadora/main.js
+++ semana06/dia03-javascript-calculadora/main.js
@@ -1,1 +1,42 @@
 console.log('Hola Javascript!')
+
+let numeroActual = '0'
+let operador = ''
+let operando = ''
+
+const inputDisplay = document.querySelector('.inputDisplay')
+const buttons = document.querySelectorAll('.button')
+
+// console.log(inputDisplay)
+// console.log(buttons)
+
+// EVENTOS
+
+buttons.forEach(function(button) {
+  // console.log(button)
+
+  button.addEventListener('click', function(event) {
+    // console.log('Me hicieron click, auxilio!')
+    // console.log(event)
+
+    const button = event.target
+    const buttonText = button.textContent
+
+    // console.log(buttonText)
+
+    if('+-*'.includes(buttonText)) {
+      //
+    } else if (buttonText === '=') {
+      //
+    } else if (buttonText === 'AC') {
+      //
+    } else { // Se presionó algún número
+      numeroActual = numeroActual + Number(buttonText)
+    }
+
+    inputDisplay.value = numeroActual
+  })
+})
+
+
+
