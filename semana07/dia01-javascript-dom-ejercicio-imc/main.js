14b6c3ffb2aa41729e2688288341c99474d6c109
Victor Villazón <victorzn23@gmail.com>
2023-11-08 20:12:53 -0500

    semana07, dia01, javasript-dom-ejercicios-imc

--- semana07/dia01-javascript-dom-ejercicio-imc/main.js
+++ semana07/dia01-javascript-dom-ejercicio-imc/main.js
@@ -1,1 +1,17 @@
-console.log('Hola desde JS')
+// console.log('Hola desde JS')
+
+const inputPeso = document.querySelector('#inputPeso')
+const inputAltura = document.querySelector('#inputAltura')
+const btnCalcular = document.querySelector('#btnCalcular')
+
+btnCalcular.addEventListener('click', function (event) {
+  // console.log(event)
+  console.log(event.target)
+})
+
+
+
+
+// console.log(inputPeso, inputAltura)
+// console.log(inputPeso.value)
+// inputPeso.value = 90
