3f45ede54433c7e6c8c56858581eadf58ef567d9
Victor Villazón <victorzn23@gmail.com>
2023-11-08 22:02:12 -0500

    semana06, dia01, javascript-dom-todo-basico

--- semana07/dia01-javascript-dom-todo-basico/main.js
+++ semana07/dia01-javascript-dom-todo-basico/main.js
@@ -1,1 +1,14 @@
-console.log('Funciona!')
+// console.log('Funciona!')
+
+const taskAdd = document.querySelector('.task__add')
+const taskInput = document.querySelector('.task__input')
+
+taskAdd.addEventListener('click', function(event) {
+  // Se ejecutará cuando hagamos click en el botón "Añadir tarea"
+
+  console.log(taskInput.value)
+
+  const button = document.createElement('button')
+  button.textContent = 'Hola soy un botón'
+  document.body.appendChild(button)
+})
