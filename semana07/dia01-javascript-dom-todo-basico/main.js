8707829426e6e448ea5747fed0a0d9f4160fe9b4
Victor Villazón <victorzn23@gmail.com>
2023-11-08 22:20:41 -0500

    semana06, dia01, javascript-dom-todo-basico

--- semana07/dia01-javascript-dom-todo-basico/main.js
+++ semana07/dia01-javascript-dom-todo-basico/main.js
@@ -1,14 +1,22 @@
 // console.log('Funciona!')
 
 const taskAdd = document.querySelector('.task__add')
-const taskInput = document.querySelector('.task__input')
+// const taskInput = document.querySelector('.task__input')
+const taskInput = document.getElementById('taskInput')
+const taskList = document.querySelector('.task__list') // UL
 
 taskAdd.addEventListener('click', function(event) {
   // Se ejecutará cuando hagamos click en el botón "Añadir tarea"
 
   console.log(taskInput.value)
 
-  const button = document.createElement('button')
-  button.textContent = 'Hola soy un botón'
-  document.body.appendChild(button)
+  // const button = document.createElement('button')
+  // button.textContent = 'Hola soy un botón'
+  // document.body.appendChild(button)
+
+  const li = document.createElement('li')
+  li.textContent = taskInput.value
+  taskList.appendChild(li)
+
+  taskInput.value = ''
 })
