c83f5c2058eb9c3748818232233070bec7b33651
Victor Villazón <victorzn23@gmail.com>
2023-11-09 19:39:17 -0500

    semana07, dia02, javascript-dom-todo-intermedio

--- semana07/dia02-javascript-dom-todo-intermedio/main.js
+++ semana07/dia02-javascript-dom-todo-intermedio/main.js
@@ -1,1 +1,9 @@
-console.log('Hola JS')
+// console.log('Hola JS')
+
+const tasks = [] // Aquí almacenaremos las tareasr
+
+const taskInput = document.querySelector('.task__input') // Es la representación de un input desde javascript
+const taskList = document.querySelector('.task__list')
+
+// console.log(taskInput, taskList)
+
