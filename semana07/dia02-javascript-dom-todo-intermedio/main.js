6b133bf603bc754507a05482d1f1d1c535d84712
Victor Villazón <victorzn23@gmail.com>
2023-11-09 20:12:21 -0500

    semana07, dia02, javascript-dom-todo-intermedio

--- semana07/dia02-javascript-dom-todo-intermedio/main.js
+++ semana07/dia02-javascript-dom-todo-intermedio/main.js
@@ -1,6 +1,6 @@
 // console.log('Hola JS')
 
-const tasks = [] // Aquí almacenaremos las tareasr
+const tasks = [] // Aquí almacenaremos las tareas
 
 const taskInput = document.querySelector('.task__input') // Es la representación de un input desde javascript
 const taskList = document.querySelector('.task__list')
@@ -10,8 +10,25 @@ const taskList = document.querySelector('.task__list')
 
 taskInput.addEventListener('keypress', function(event) {
   // Lógica de la aplicación
-  console.log(event.key)
+  // console.log(event.key)
   if (event.key === 'Enter') {
-    console.log('Agregando una nueva tarea...')
+    // console.log('Agregando una nueva tarea...')
+    const input = event.target
+    const value = input.value
+
+    const newTask = {
+      title: value,
+      done: false
+    }
+
+    // console.log(newTask)
+
+    // console.log(tasks)
+
+    tasks.push(newTask)
+
+    // console.log(tasks)
+
+    taskInput.value = ''
   }
 })
