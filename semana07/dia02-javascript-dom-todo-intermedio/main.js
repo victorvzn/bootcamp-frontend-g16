56a68c722b4b6f3a16ca7877ce6b0e9d94fd6038
Victor Villazón <victorzn23@gmail.com>
2023-11-09 19:50:35 -0500

    semana07, dia02, javascript-dom-todo-intermedio

--- semana07/dia02-javascript-dom-todo-intermedio/main.js
+++ semana07/dia02-javascript-dom-todo-intermedio/main.js
@@ -6,4 +6,12 @@ const taskInput = document.querySelector('.task__input') // Es la representació
 const taskList = document.querySelector('.task__list')
 
 // console.log(taskInput, taskList)
+// console.log({ taskInput, taskList })
 
+taskInput.addEventListener('keypress', function(event) {
+  // Lógica de la aplicación
+  console.log(event.key)
+  if (event.key === 'Enter') {
+    console.log('Agregando una nueva tarea...')
+  }
+})
