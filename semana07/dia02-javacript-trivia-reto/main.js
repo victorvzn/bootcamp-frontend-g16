c9f8fcc95da26879cde88dd97a83b40aee62f3f9
Victor Villazón <victorzn23@gmail.com>
2023-11-10 19:23:46 -0500

    semana07, dia03, javascript-trivia

--- semana07/dia02-javacript-trivia-reto/main.js
+++ semana07/dia02-javacript-trivia-reto/main.js
@@ -1,1 +1,5 @@
-console.log('Trivia OK!')
+// console.log('Trivia OK!')
+
+// const questionsAndResults = document.querySelector('#questions-and-results')
+const questionsAndResults = document.getElementById('questions-and-results')
+
