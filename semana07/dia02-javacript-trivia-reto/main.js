b4cf5072e327e6d15fcb27a97f6ab89384bf56fb
Victor Villazón <victorzn23@gmail.com>
2023-11-10 19:58:20 -0500

    semana07, dia03, javascript-trivia

--- semana07/dia02-javacript-trivia-reto/main.js
+++ semana07/dia02-javacript-trivia-reto/main.js
@@ -15,16 +15,60 @@ const questions = [
   },
   {
     id: 3,
-    question: '¿Cuál es el función en JS que se utiliza para redondear hacia abajo el valor de un número decimal?',
+    question: '¿Cuál es la función en JS que se utiliza para redondear hacia abajo el valor de un número decimal?',
     correctAnswer: 0,
     answerList: ['floor()', 'ceil()', 'round()']
   }
 ]
 
-const currentQuestionIndex = 0
-
-console.log(questions[currentQuestionIndex])
-
 // const questionsAndResults = document.querySelector('#questions-and-results')
 const questionsAndResults = document.getElementById('questions-and-results')
+
+const currentQuestionIndex = 0
+// console.log(questions[currentQuestionIndex])
+
+function renderQuestions() {
+  // console.log('Estoy renderizando las preguntas...')
+  const currentQuestion = questions[currentQuestionIndex]
+
+  const question = `
+    <section class="flex flex-col px-4 py-6 mb-3 border rounded-lg shadow">
+      <p class="text-md font-medium text-gray-900 mb-4">${currentQuestion.question}</p>
+
+      <div class="flex flex-col items-start mb-10">
+        <button
+          type="button"
+          class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-left mr-2 mb-2 w-full"
+        >
+          ${currentQuestion.answerList[0]}
+        </button>
+        <button
+          type="button"
+          class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-left mr-2 mb-2 w-full"
+        >
+          ${currentQuestion.answerList[1]}
+        </button>
+        <button
+          type="button"
+          class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-left mr-2 mb-2 w-full"
+        >
+          ${currentQuestion.answerList[2]}
+        </button>
+        <button type="button" class="hidden text-white border border-red-600 bg-red-600 font-medium rounded-lg text-sm px-5 py-2.5 text-left mr-2 mb-2 w-full">Respuesta incorrecta 2</button>
+        <button type="button" class="hidden text-white border border-green-600 bg-green-600 font-medium rounded-lg text-sm px-5 py-2.5 text-left mr-2 mb-2 w-full">Respuesta correcta 3</button>
+      </div>
+
+      <div class="flex justify-end">
+        <button type="button" class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Siguiente pregunta</button>
+        <button type="button" class="text-white border border-blue-300 bg-blue-600 font-medium rounded-lg text-sm px-5 py-2.5 text-left mr-2 mb-2">Mostrar resultados</button>
+      </div>
+    </section>
+  `
+
+  questionsAndResults.innerHTML = question
+}
+
+renderQuestions()
+
+
 
