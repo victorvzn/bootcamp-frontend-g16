37e20e38a9022b650da932b7f2e2a5e4fe6e72ad
Victor Villazón <victorzn23@gmail.com>
2023-11-15 21:57:25 -0500

    semana08, dia01, javascript-es6-fetch-simple

--- semana08/dia01-javascript-es6-fetch-simple/main.js
+++ semana08/dia01-javascript-es6-fetch-simple/main.js
@@ -4,8 +4,43 @@ console.log('Hola JS!')
 
 const url = 'https://jsonplaceholder.typicode.com/posts'
 
-fetch(url) // Devuelve una promesa (Promise)
-  .then(response => response.json()) // Convertimos la respuesta a un objeto JS
-  .then(data => {
-    console.log(data)
-  }) // Podemos usar el objeto JS
+// fetch(url) // Devuelve una promesa (Promise)
+//   .then(response => response.json()) // Convertimos la respuesta a un objeto JS
+//   .then(data => {
+//     console.log(data)
+//   }) // Podemos usar el objeto JS
+
+const fetchPosts = () => {
+  console.log('Llamando al api de JSONPLACEHOLDER')
+
+  return fetch(url)
+    .then(response => response.json())
+    // .then(data => {
+    //   return data
+    // })
+}
+
+const renderPosts = (posts = []) => {
+  // const appDiv = document.querySelector('#app')
+  const appDiv = document.getElementById('app')
+
+  let postList = ''
+
+  posts.forEach(post => {
+    // console.log(post)
+    postList += `
+      <div>
+        <h2>${post.id}: ${post.title}</h2>
+        <p>${post.body}</p>
+      </div>
+    `
+  })
+
+  appDiv.innerHTML = postList
+}
+
+fetchPosts()
+  .then(posts => {
+    renderPosts(posts)
+    console.log(posts)
+  })
