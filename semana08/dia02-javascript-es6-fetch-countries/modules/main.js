02aa95e9231af4b9c77360b8658b54f7ed7c4af6
Victor Villazón <victorzn23@gmail.com>
2023-11-16 21:37:00 -0500

    semana08, dia02, javascript-es6-fetch-countries

--- semana08/dia02-javascript-es6-fetch-countries/modules/main.js
+++ semana08/dia02-javascript-es6-fetch-countries/modules/main.js
@@ -1,1 +1,33 @@
-console.log('Hola JS!')
+// console.log('Hola JS!')
+
+const url = 'https://restcountries.com/v3.1/all?fields=name,flags,capital,population,languages,currencies,timezones'
+
+// function fetchCountries() {
+//   return fetch(url) // Promise
+//     .then(response => response.json())
+//     .catch(error => console.log(error))
+// }
+
+// TODO: Reescribir la función fetchCountries usando async/await para luego del receso.
+
+const fetchCountries = async () => {
+  try {
+    const response = await fetch(url) // Promise
+
+    if(!response.ok) {
+      throw new Error('ERROR:', response.status)
+    }
+
+    const json = await response.json()
+
+    return json
+  } catch(error) {
+    console.log(error)
+  }
+}
+
+fetchCountries()
+  .then(data => console.log(data))
+
+
+
