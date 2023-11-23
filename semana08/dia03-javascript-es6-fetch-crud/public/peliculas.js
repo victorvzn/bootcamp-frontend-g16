521b007765d92580d7fab95a41817b01b827c2d2
Victor Villazón <victorzn23@gmail.com>
2023-11-22 20:56:22 -0500

    semana08, dia03, javascript-es6-fetch-simple-modules. Continuación en la semana09

--- semana08/dia03-javascript-es6-fetch-crud/public/peliculas.js
+++ semana08/dia03-javascript-es6-fetch-crud/public/peliculas.js
@@ -1,7 +1,35 @@
 export const renderPeliculas = (peliculas) => {
   const peliculasList = document.querySelector('.peliculas__list')
 
-  let elementos = '<h1>Este es el listado de peliculas</h1>'
+  let elementos = ''
+
+  peliculas.forEach(pelicula => {
+    elementos += `
+      <tr>
+        <td>${pelicula.id}</td>
+        <td>
+          <img
+            src="${pelicula.imagen}"
+            width="100"
+            height="250"
+          />
+        </td>
+        <td>
+          <strong>${pelicula.nombre}</strong>
+          <div class="fs-small">
+            <strong>Estreno:</strong> ${pelicula.estreno}
+          </div>
+          <div class="fs-small">
+            <strong>Genero:</strong> ${pelicula.generoId}
+          </div>
+          <div class="fs-small">
+            <strong>Resumen:</strong> ${pelicula.resumen}
+          </div>
+        </td>
+        <td>***</td>
+      </tr>
+    `
+  })
 
   peliculasList.innerHTML = elementos
 }
