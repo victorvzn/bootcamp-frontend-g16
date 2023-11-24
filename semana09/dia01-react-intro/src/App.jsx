fa2c8a0b73526da5498b40ac6eeaf504d910f8fc
Victor Villazón <victorzn23@gmail.com>
2023-11-23 20:09:32 -0500

    semana09, dia01, react-intro

--- semana09/dia01-react-intro/src/App.jsx
+++ semana09/dia01-react-intro/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// COMPONENTE USANDO FUNCTION
 
+// function App() {
+//   return <h1>Hola React!</h1>
+// }
+
+// COMPONENTE USANDO ARROW FUNCTIONS
+
+// const App = () => {
+//   return <h1>Hola React!</h1>
+// }
+
+// COMPONENTE DE RECT USANDO MULTIPLES LÍNEAS CON SU ELEMENTO PADRE
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Hola React!</h1>
+//       <p>Estoy en el bootcamp!</p>
+//     </div>
+//   )
+// }
+
+// COMPONENTE USANDO FRAGMENTOS (FRAGMENTS) PARA EVITAR RENDERIZAR EN UN ELEMENTO PADRE
+
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hola React!</h1>
+      <p>Estoy en el bootcamp!</p>
     </>
   )
 }
