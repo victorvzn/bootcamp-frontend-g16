9340cf7bc8960ff7bbe37a8e31035912845fe0fa
Victor Villazón <victorzn23@gmail.com>
2023-12-06 20:01:54 -0500

    semana11, dia01, react-router-components

--- semana11/dia01-react-router-components/src/App.jsx
+++ semana11/dia01-react-router-components/src/App.jsx
@@ -2,13 +2,18 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
-import { BrowserRouter, Routes, Route  } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link  } from "react-router-dom";
 
 export default function App() {
   return (
     <BrowserRouter>
       <header>
-
+        <h1>React router DOM - Components</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </nav>
       </header>
       <main>
         <Routes>
@@ -17,7 +22,7 @@ export default function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
         </Routes>
-        </main>
+      </main>
     </BrowserRouter>
   )
 }
