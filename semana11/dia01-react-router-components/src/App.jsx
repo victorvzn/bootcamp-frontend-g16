84ecf119cb59dd3b443056c1dfd052a93f32c0bb
Carlos Branty <losbrantybc@yahoo.com>
2023-12-06 21:41:54 -0500

    Creacion de Route

--- semana11/dia01-react-router-components/src/App.jsx
+++ semana11/dia01-react-router-components/src/App.jsx
@@ -4,18 +4,20 @@ import Register from "./pages/Register";
 import LayoutBase from "./layouts/LayoutBase";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CarlosConza from "./pages/alumnos/CarlosConza";
 
 export default function App() {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LayoutBase />}>
-            <Route index element={<Home />} />
+      <Routes>
+        <Route path="/" element={<LayoutBase />}>
+          <Route index element={<Home />} />
 
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-          </Route>
-        </Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/carlos" element={<CarlosConza />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
