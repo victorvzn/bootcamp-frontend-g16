0ee75ae3c9fc0412f85349ad6fd79428f31f506d
Victor Villazón <victorzn23@gmail.com>
2023-12-07 20:24:27 -0500

    semana11, dia02, react-router-invoice-app

--- semana11/dia02-react-router-invoice-app/src/pages/Invoices.jsx
+++ semana11/dia02-react-router-invoice-app/src/pages/Invoices.jsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react"
 import Header from "../components/Header"
 import InvoiceList from "../components/invoices/InvoiceList"
+import { fetchInvoices } from "../services/invoices"
 
-const Invoices = ({ invoices }) => {
+const Invoices = () => {
+  const [invoices, setInvoices] = useState([])
+  
+  useEffect(() => {
+    fetchInvoices()
+      .then(invoices => setInvoices(invoices))
+  }, [])
+
   return (
     <>
       <Header title='Invoices' />
