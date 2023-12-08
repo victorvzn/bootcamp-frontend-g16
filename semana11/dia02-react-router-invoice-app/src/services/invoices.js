834a2a133567370d8b1dd2fd0f926fe52300df6a
Victor Villazón <victorzn23@gmail.com>
2023-12-07 20:16:46 -0500

    semana11, dia02, react-router-invoice-app

--- semana11/dia02-react-router-invoice-app/src/services/invoices.js
+++ semana11/dia02-react-router-invoice-app/src/services/invoices.js
@@ -1,7 +1,8 @@
-const API_URL = ''
+const API_URL = 'https://raw.githubusercontent.com/victorvzn/bootcamp-frontend-g16/main/semana11/dia02-react-router-invoice-app/public/invoices.json'
 
 export const fetchInvoices = async () => {
-  const url = `${API_URL}/invoices`
+  // const url = `${API_URL}/invoices`
+  const url = `${API_URL}`
 
   const response = await fetch(url)
 
