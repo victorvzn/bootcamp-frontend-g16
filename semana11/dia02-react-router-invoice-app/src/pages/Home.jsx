614b19ce1254e63b4727ab727a0857d4cab97433
Victor Villazón <victorzn23@gmail.com>
2023-12-08 23:04:52 -0500

    semana11, dia02, react-router-invoice-app

--- semana11/dia02-react-router-invoice-app/src/pages/Home.jsx
+++ semana11/dia02-react-router-invoice-app/src/pages/Home.jsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react"
+
 const Home = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    // const url = 'https://dummyjson.com/products'
+    const url = 'https://dummyjson.com/auth/products'
+
+    const userFromLS = JSON.parse(localStorage.getItem('auth'))
+
+    const token = userFromLS.token
+
+    const options = {
+      headers: {
+        'Authorization': `Bearer ${token}`, 
+        'Content-Type': 'application/json'
+      }
+    }
+
+    fetch(url, options)
+      .then(response => response.json())
+      .then(data => setProducts(data.products))
+  }, [])
+
   return (
-    <div>Home</div>
+    <main className="w-[700px] m-auto flex flex-col gap-5">
+      <div className="bg-slate-600 p-8 rounded-lg flex flex-col gap-6">
+        <h2 className="text-white text-center text-4xl font-bold">Invoice app</h2>
+
+        <pre>
+          {JSON.stringify(products, null, 2)}
+        </pre>
+      </div>
+    </main>
   )
 }
 
