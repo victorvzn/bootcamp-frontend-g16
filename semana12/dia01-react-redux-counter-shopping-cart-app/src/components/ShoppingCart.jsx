19a2e4369fd893e95927740cee8ef10de6109dcf
Victor Villazón <victorzn23@gmail.com>
2023-12-13 22:19:41 -0500

    semana12, dia01, react-redux-counter-shopping-cart-app

--- semana12/dia01-react-redux-counter-shopping-cart-app/src/components/ShoppingCart.jsx
+++ semana12/dia01-react-redux-counter-shopping-cart-app/src/components/ShoppingCart.jsx
@@ -7,7 +7,20 @@ const ShoppingCart = () => {
     <div>
       <h3>Shopping Cart</h3>
 
-      <pre>{JSON.stringify(cart, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(cart, null, 2)}</pre> */}
+
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+        {cart && cart.map(product => {
+          return (
+            <button
+              key={product.id}
+              style={{ backgroundColor: '#FF8080' }}
+            >
+              {product.title} - S/ {product.price}
+            </button>
+          )
+        }) }
+      </div>
     </div>
   )
 }
