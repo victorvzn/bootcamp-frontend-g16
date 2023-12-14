c48ffbe4633da9e4b23526f58a3f3f41bd28da2d
Victor Villazón <victorzn23@gmail.com>
2023-12-13 19:52:00 -0500

    semana12, dia01, react-redux-counter-shopping-cart-app

---
+++ semana12/dia01-react-redux-counter-shopping-cart-app/src/store/index.js
@@ -0,0 +1,26 @@
+// PASO-01: Creamos un store
+
+import { configureStore } from "@reduxjs/toolkit"
+
+import { createSlice } from "@reduxjs/toolkit"
+
+// Reducer: son funciones que nos ayudan a transformar los estados que creamos.
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState: 0,
+  reducers: {
+    increment: (state) => {
+      return state + 1
+    },
+    decrement: (state) => state - 1
+  }
+})
+
+export const { increment, decrement } = counterSlice.actions // Exportamos las acciones
+
+export const store = configureStore({
+  reducer: {
+    counter: counterSlice // Exportando el estado inicial y a los reducers del slice
+  }
+})
