942f8a9c0f6d9e044a064d2b68cfd6e43e66cc2c
Victor Villazón <victorzn23@gmail.com>
2023-12-14 21:02:45 -0500

    semana12, dia02, react-budget-app-custom-hooks-context

--- semana12/dia02-react-budget-app-custom-hooks-context/src/pages/Register.jsx
+++ semana12/dia02-react-budget-app-custom-hooks-context/src/pages/Register.jsx
@@ -1,6 +1,46 @@
 const Register = () => {
   return (
-    <div>Register Page</div>
+    <form className="w-96 mx-auto">
+      <h1 className="text-center text-3xl mb-8">Budget App - Register</h1>
+
+      <div className="flex flex-col gap-4 bg-slate-200 p-8 shadow-lg rounded-lg">
+        <label className="font-medium">
+          Full name
+          <input
+            type="text"
+            name="fullname"
+            paceholder="Jhon Doe"
+            className="border w-full p-3"
+            required
+          />
+        </label>
+        <label className="font-medium">
+          Email
+          <input
+            type="text"
+            name="email"
+            paceholder="jhon_doe@budgetapp.com"
+            className="border w-full p-3"
+            required
+          />
+        </label>
+        <label className="font-medium">
+          Password
+          <input
+            type="text"
+            name="password"
+            paceholder="Sup3rSecre3tPass"
+            className="border w-full p-3"
+            required
+          />
+        </label>
+        <input
+          type="submit"
+          value='Register'
+          className="w-full bg-amber-400 p-3 font-medium"
+        />
+      </div>
+    </form>
   )
 }
 
