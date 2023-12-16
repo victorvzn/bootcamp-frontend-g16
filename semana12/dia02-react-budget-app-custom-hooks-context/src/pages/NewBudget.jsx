12f039a5b23496b425e9d7233c8b506c88edf09a
Victor Villazón <victorzn23@gmail.com>
2023-12-15 22:01:51 -0500

    semana12, dia03, react-budget-app-custom-hooks-context

--- semana12/dia02-react-budget-app-custom-hooks-context/src/pages/NewBudget.jsx
+++ semana12/dia02-react-budget-app-custom-hooks-context/src/pages/NewBudget.jsx
@@ -1,6 +1,12 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
+
+import { UserContext } from '../context/UserContext'
+
+import { Toaster, toast } from 'sonner'
 
 const NewBudget = () => {
+  const { user } = useContext(UserContext)
+
   const [form, setForm] = useState({
     budget: ''
   })
@@ -11,17 +17,42 @@ const NewBudget = () => {
     setForm({ ...form, [name]: value })
   }
 
-  const handleSaveBudget =(event) => {
+  const handleSaveBudget = async (event) => {
     event.preventDefault();
     
     console.log('Estoy por guardar la data...')
+
+    const { budget } = form
+
+    const userId = user.id
+
+    const url = `https://65728910d61ba6fcc0152a3d.mockapi.io/api/v1/users/${userId}`
+
+    const options = {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({ budget: Number(budget) })
+    }
+
+    const response = await fetch(url, options)
+
+    const data = await response.json()
+
+    console.log(data)
+
+    toast.success('Budget saved!')
   }
 
   return (
     <form className="w-96 mx-auto" onSubmit={handleSaveBudget}>
+      <Toaster richColors />
+
       <h1 className="text-center text-3xl mb-8">Budget App - New budget</h1>
 
       {JSON.stringify(form)}
+      {JSON.stringify(user)}
 
       <div className="flex flex-col gap-4 bg-slate-200 p-8 shadow-lg">
         <label className="font-medium">
