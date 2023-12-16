1538e33b05aaa2f46cef63ff2a7412a4faa32359
Victor Villazón <victorzn23@gmail.com>
2023-12-15 20:11:12 -0500

    semana12, dia03, react-budget-app-custom-hooks-context

--- semana12/dia02-react-budget-app-custom-hooks-context/src/layouts/LayoutBase.jsx
+++ semana12/dia02-react-budget-app-custom-hooks-context/src/layouts/LayoutBase.jsx
@@ -1,16 +1,41 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useNavigate } from 'react-router-dom'
+
+import useAuth from '../hooks/useAuth'
 
 const LayoutBase = () => {
+  const { isAuth, logout } = useAuth()
+
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    logout()
+  
+    navigate('/login')
+  }
+
   return (
     <>
       <header className='bg-amber-200 py-4 px-6'>
         <div className='container mx-auto flex justify-between'>
           <h1 className='font-bold'>Budget App</h1>
           <nav className='flex gap-3'>
-            <Link to='/'>Home</Link>
-            <Link to='/login'>Login</Link>
-            <Link to='/register'>Register</Link>
+            {isAuth && (
+              <>
+                <Link to='/'>Home</Link>
+                <button
+                  className='text-red-800 font-medium'
+                  onClick={handleLogout}
+                >Logout</button>
+              </>
+            )}
+
+            {!isAuth && (
+              <>
+                <Link to='/login'>Login</Link>
+                <Link to='/register'>Register</Link>
+              </>
+            )}
           </nav>
         </div>
       </header>
