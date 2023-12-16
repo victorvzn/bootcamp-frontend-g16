de6c666264af0df49f2f64c7e098d55db1ca6182
Victor Villazón <victorzn23@gmail.com>
2023-12-15 21:27:54 -0500

    semana12, dia03, react-budget-app-custom-hooks-context

--- semana12/dia02-react-budget-app-custom-hooks-context/src/pages/NewBudget.jsx
+++ semana12/dia02-react-budget-app-custom-hooks-context/src/pages/NewBudget.jsx
@@ -1,6 +1,47 @@
+import { useState } from "react"
+
 const NewBudget = () => {
+  const [form, setForm] = useState({
+    budget: ''
+  })
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSaveBudget =(event) => {
+    event.preventDefault();
+    
+    console.log('Estoy por guardar la data...')
+  }
+
   return (
-    <div>New Budget Page</div>
+    <form className="w-96 mx-auto" onSubmit={handleSaveBudget}>
+      <h1 className="text-center text-3xl mb-8">Budget App - New budget</h1>
+
+      {JSON.stringify(form)}
+
+      <div className="flex flex-col gap-4 bg-slate-200 p-8 shadow-lg">
+        <label className="font-medium">
+          Budget
+          <input
+            type="number"
+            name="budget"
+            placeholder="0"
+            className="border w-full p-3"
+            onChange={handleChange}
+            value={form.budget}
+          />
+        </label>
+        <input
+          type="submit"
+          value="Save Budget"
+          className="w-full bg-amber-300 p-3 font-medium"
+        />
+      </div>
+    </form>
   )
 }
 
